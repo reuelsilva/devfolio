db8ca5f6096d5a74864b389dce31bb57d44e83a9
reuelsilva <reuelsilva19@gmail.com>
2024-01-17 11:55:02 -0300

    feat: add Header component

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+
+import "./style.scss"
+
+export function Header(){
+    return(
+        <div className="header">
+            <div>
+                <h1>Olá, sou o dev Reuel! 👋</h1>
+                <h2>Desenvolvedor Front-end Junior</h2>
+            </div>
+
+            <Image
+                src="https://avatars.githubusercontent.com/u/109824495?v=4"
+                alt=""
+                width={250}
+                height={250}
+                priority
+            />
+      </div>
+    )
+}
