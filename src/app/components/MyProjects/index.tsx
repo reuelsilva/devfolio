c7f784d2e157e1927eb4e2039ed8bba6a0637668
reuelsilva <reuelsilva19@gmail.com>
2024-04-22 19:23:25 -0300

    alterado as props de um componente ProjectItem

--- src/app/components/MyProjects/index.tsx
+++ src/app/components/MyProjects/index.tsx
@@ -5,10 +5,10 @@ export function MyProjects(){
     return(
         <div className="projects-section">
             <h3>Meus Projetos</h3>
-            <div className="projects-grid">
+            <div className="projects-grid">               
+                <ProjectItem title="projeto-blog" description="Projeto de blog Curiosidades de Tecnologia. É um site estático e com navegação entre rotas." skillsUsed={["React", "TypeScript", "Tailwind CSS", "JavaScript"]} repo_url="https://github.com/reuelsilva/tech-blog" deploy_url="https://tech-blog-page.vercel.app/"/>
+
                 <ProjectItem title="netflix-clone" description="Replicando a interface da Netflix com responsividade e APIs" skillsUsed={["React", "JavaScript", "HTML", "CSS"]} repo_url="https://github.com/reuelsilva/netflix-clone" deploy_url="https://reuelsilva.github.io/netflix-clone/"/>
-                
-                <ProjectItem title="projeto-android" description="Site responsivo com artigo 'Historia do Mascote do Android'" skillsUsed={["JavaScript", "HTML", "CSS", "Tailwind CSS"]} repo_url="https://github.com/reuelsilva/projeto-android" deploy_url="https://reuelsilva.github.io/projeto-android/"/>
             </div>
         </div>
     )
