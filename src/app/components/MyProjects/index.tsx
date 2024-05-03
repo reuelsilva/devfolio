f82aef86f08eb08297e4b8346e948e0275ced229
reuelsilva <reuelsilva19@gmail.com>
2024-05-03 12:01:13 -0300

    refactor: alterando o texto da props description do componente ProjectItem

--- src/app/components/MyProjects/index.tsx
+++ src/app/components/MyProjects/index.tsx
@@ -8,7 +8,7 @@ export function MyProjects(){
             <div className="projects-grid">               
                 <ProjectItem title="projeto-blog" description="Projeto de blog para artigos de tecnologia com navegação entre rotas." skillsUsed={["NextJS", "React", "TypeScript", "Tailwind CSS", "JavaScript"]} repo_url="https://github.com/reuelsilva/projeto-blog" deploy_url="https://curiosidades-de-tecnologia.vercel.app/"/>
 
-                <ProjectItem title="mks-loja-virtual" description="Projeto de loja virtual renderizando a lista de produtos de uma API e exibindo o carrinho de compras." skillsUsed={["NextJS", "React", "TypeScript", "JavaScript", "Sass"]} repo_url="https://github.com/reuelsilva/mks-frontend-challenge" deploy_url="https://mks-frontend-challenge-deploy.vercel.app/" />
+                <ProjectItem title="mks-loja-virtual" description="Projeto de loja virtual renderizando a lista de produtos de uma API e exibindo um carrinho de compras interativo." skillsUsed={["NextJS", "React", "TypeScript", "JavaScript", "Sass"]} repo_url="https://github.com/reuelsilva/mks-frontend-challenge" deploy_url="https://mks-frontend-challenge-deploy.vercel.app/" />
 
                 <ProjectItem title="netflix-clone" description="Replicando a interface da Netflix com responsividade e APIs" skillsUsed={["React", "JavaScript", "HTML", "CSS"]} repo_url="https://github.com/reuelsilva/netflix-clone" deploy_url="https://reuelsilva.github.io/netflix-clone/"/>
             </div>
