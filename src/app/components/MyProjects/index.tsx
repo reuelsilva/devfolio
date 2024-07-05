c1db29329ce13758722bf50819b90a8002fec4f5
reuelsilva <reuelsilva19@gmail.com>
2024-07-04 22:51:14 -0300

    alterando url do deploy

--- src/app/components/MyProjects/index.tsx
+++ src/app/components/MyProjects/index.tsx
@@ -12,7 +12,7 @@ export function MyProjects(){
 
                 <ProjectItem title="coffee-shop" description="Projeto de site de uma cafeteria desenvolvido com o back-end NodeJS para alimentar os dados do front-end." skillsUsed={["NextJS", "React", "JavaScript", "TypeScript", "CSS", "HTML"]} repo_url="https://github.com/reuelsilva/coffee-shop" deploy_url="#" />
 
-                <ProjectItem title="weather-app" description="Projeto de uma aplicação de busca de dados do clima." skillsUsed={["HTML", "JavaScript", "CSS"]} repo_url="https://github.com/reuelsilva/weather-app" deploy_url="https://reuelsilva.github.io/weather-app/src/index.html" />
+                <ProjectItem title="weather-app" description="Projeto de uma aplicação de busca de dados do clima." skillsUsed={["HTML", "JavaScript", "CSS"]} repo_url="https://github.com/reuelsilva/weather-app" deploy_url="https://reuelsilva.github.io/weather-app/" />
 
                 <ProjectItem title="netflix-clone" description="Replicando a interface da Netflix com responsividade e APIs" skillsUsed={["React", "NextJS", "JavaScript", "Tailwind CSS", "HTML"]} repo_url="https://github.com/reuelsilva/netflix-clone" deploy_url="https://netflix-clone-develop.vercel.app/"/>
             </div>
