e76511f3578322c82df72c4440b69939f69dd281
reuelsilva <reuelsilva19@gmail.com>
2024-10-02 18:57:44 -0300

    feat: adicionado novo componente Bio

---
+++ src/app/components/AboutMe/Bio/index.jsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+
+export function Bio() {
+    return (
+        <div className="bio-area">
+            <h3>Sobre Mim</h3>
+            <p>Olá! Meu nome é Reuel Silva, tenho 25 anos e sou desenvolvedor Front-end em nivel Junior. Atualmente dedico meu tempo a criar projetos pessoais que me ajudam a aprimorar minhas habilidades e expandir meu conhecimento na área. O mundo da programação me fascina e nele pude encontrar algo que entenda e faça com paixão.</p>
+        </div>
+    )
+}
