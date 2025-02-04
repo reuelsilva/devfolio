259910d0df73cceba0c0b792287457fcd85c2b5d
reuelsilva <reuelsilva19@gmail.com>
2025-02-04 10:35:49 -0300

    fix: adiciona classe "hidden" ao Link para esconder o botão no mobile
    - A classe "hidden" foi adicionada ao componente Link para garantir que o ícone de seta só apareça em telas maiores (sm e acima).

--- src/app/components/Main/Home/ArrowDownButton/index.tsx
+++ src/app/components/Main/Home/ArrowDownButton/index.tsx
@@ -3,7 +3,7 @@ import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrow
 
 export default function ArrowDownButton() {
     return (
-        <Link href="#projects">
+        <Link href="#projects" className="hidden sm:inline-block">
             <KeyboardDoubleArrowDownIcon 
                 style={{ fontSize: '48px' }} 
                 className="hidden sm:inline-block sm:absolute sm:left-1/2 sm:bottom-4 sm:animate-arrowUpAndDown text-[#878EA1]" 
