b06a5d2d6cc6bc6617d6b6faf3b06f0d5bff4d09
reuelsilva <reuelsilva19@gmail.com>
2025-03-27 09:55:09 -0300

    fix: adiciona target="_blank" na tag <a> para abrir links externos

--- src/app/components/Main/Contacts/SocialLink/index.tsx
+++ src/app/components/Main/Contacts/SocialLink/index.tsx
@@ -9,7 +9,7 @@ type SocialLinkProps = {
 
 export default function SocialLink(props: SocialLinkProps) {
     return (
-        <a href={props.href} className='flex items-center gap-3 w-full bg-color-05 p-4 rounded-lg'>
+        <a href={props.href} target="_blank" className='flex items-center gap-3 w-full bg-color-05 p-4 rounded-lg'>
             <Image src={props.icon} alt="" width={30} height={30} />
             <span className='text-color-03 font-medium'>{props.social}</span>
             <ArrowUpRight/>
